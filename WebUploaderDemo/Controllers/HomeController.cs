a5744cf2fd4d559114fa89a37e3e7e45bf6a22d0
wei.zhou <554961776@qq.com>
2020-08-13 22:37:29 +0800

    多图片上传demo

--- WebUploaderDemo/Controllers/HomeController.cs
+++ WebUploaderDemo/Controllers/HomeController.cs
@@ -9,6 +9,59 @@ namespace WebUploaderDemo.Controllers
 {
     public class HomeController : Controller
     {
+        static string urlPath = string.Empty;
+        public HomeController()
+        {
+            var applicationPath = VirtualPathUtility.ToAbsolute("~") == "/" ? "" : VirtualPathUtility.ToAbsolute("~");
+            urlPath = applicationPath + "/Upload";
+
+        }
+        /// <summary>
+        /// 多图片上传demo
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Pic()
+        {
+            
+            return View();
+        }
+        /// <summary>
+        /// 多图片上传demo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="lastModifiedDate"></param>
+        /// <param name="size"></param>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public ActionResult UpLoadProcess(string id, string name, string type, string lastModifiedDate, int size, HttpPostedFileBase file)
+        {
+            string filePathName = string.Empty;
+
+            string localPath = Path.Combine(HttpRuntime.AppDomainAppPath, "Upload");
+            if (Request.Files.Count == 0)
+            {
+                return Json(new { jsonrpc = 2.0, error = new { code = 102, message = "保存失败" }, id = "id" });
+            }
+
+            string ex = Path.GetExtension(file.FileName);
+            filePathName = Guid.NewGuid().ToString("N") + ex;
+            if (!System.IO.Directory.Exists(localPath))
+            {
+                System.IO.Directory.CreateDirectory(localPath);
+            }
+            file.SaveAs(Path.Combine(localPath, filePathName));
+
+            return Json(new
+            {
+                jsonrpc = "2.0",
+                id = id,
+                filePath = urlPath + "/" + filePathName
+            });
+
+        }
+
         public ActionResult Index(string uploadhidden2)
         {
             ViewBag.uploadhidden2 = uploadhidden2;
